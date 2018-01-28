71fa9cca78c8db4fed6e8d0a3415ec314b318dc7
bart segers <bart.segers@febelco.be>
2018-01-28 17:21:03 +0100

    Add project files.

---
+++ AngularJSVS1/Modules.js
@@ -0,0 +1,41 @@
+﻿(function () {
+
+    //instantiëring van de hoofdmodule
+    var appAngular = angular.module("MainModule", [])
+
+    var MainController = function (s, h) {
+
+
+        //Functie die wordt uitgevoerd als gevolg van de promesse methode dat uitgevoerd wordt met de response die terugkomt van de api return
+       
+        var onSuccess = function (response) {
+            s.success = response.data;
+        }
+
+        var onError = function (response) {
+            s.error = 'Could not fetch the user';
+        }
+
+
+        h.get("https://api.github.com/users/segersbrt").then(onSuccess, onError);
+
+
+
+        var person = {
+            firstName: "Bart",
+            lastName: "Segers",
+            imageSource: "http://bartsimpsonpictures.squarelogic.net/bart-simpson-01.gif"
+        }
+
+        s.message = "Hello Angular";
+       
+        s.person = person;
+
+    }
+
+        
+    appAngular.controller("MainController", ["$scope", "$http", MainController])
+
+
+}());
+
